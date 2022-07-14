0bd611ca5e2148ed932748d411bedc8ea4fa6223
robin00007 <76676218+robin00007@users.noreply.github.com>
2022-07-14 16:37:37 +0530

    file structure changed

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDm1zuHTwD89ZcNh0KWhuucZt4B1M9IuJI",
+  authDomain: "coding-combo.firebaseapp.com",
+  projectId: "coding-combo",
+  storageBucket: "coding-combo.appspot.com",
+  messagingSenderId: "515474576018",
+  appId: "1:515474576018:web:0e892b50fc3250224dcc4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
